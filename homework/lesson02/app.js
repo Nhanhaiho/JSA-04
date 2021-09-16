64427e9b455a364c1eb28dcba07b8f831989d756
Nhanhaiho <hainhan233@gmail.com>
2021-09-16 20:29:47 +0700

    bai tap

--- homework/lesson02/app.js
+++ homework/lesson02/app.js
@@ -1,61 +1,35 @@
-// 7
+//1. Return the Remainder from Two Numbers
+function remainder(a1, b1) {
+    return a1 % b1
+}
+//console.log(remainder(5,5))
+
+//2. Find the Perimeter of a Rectangle
+function findPerimeter(length, width) {
+    return 2*(length+width)
+}
+//console.log(findPerimeter(6,7))
+
+//3. Basketball Points
 function points(point1, point2) {
     return point1 * 2 + point2 * 3;
 }
-console.log("question 7 :",points(38, 8));
+//console.log(points(38, 8));
 
-//8
-function lessThan100(number1, number2) {
-    if (number1 + number2 < 100) {
-        return true
-    } else {
-        return false;
-    }
-};
-console.log("question 8 :",lessThan100(50, 81));
-
-//9 
+//4. The Farm Problem
 function animals(ani1, ani2, ani3) {
-    let total = ani1 * 2 + ani2 * 4 + ani3 * 4;
-    return total;
+     return ani1 * 2 + ani2 * 4 + ani3 * 4;
+    
 };
-console.log("question 9 :",animals(5, 2, 8));
+//console.log(animals(5, 2, 8));
 
-//10 
-function boolToString(flag) {
-    return flag.toString();
-}
-console.log("question 10 :",boolToString(true))
-
-//11 
-function isSameNum(num1,num2) {
-    if (num1 === num2) {
-        return true
-    } else {
-        return false
-    }
-}
-console.log("question 11 :", isSameNum(2, 2))
-
-//12 
+//5. Football Points
 function footballPoints(wins, draws, losses) {
     return wins * 3 + draws * 1 + losses * 0;
 };
-console.log("question 12 :", footballPoints(5, 0, 2));
+console.log( footballPoints(5, 0, 2));
 
-//13 
-function sumPolygon(angles) {
-    return angles * 60;
-}
-console.log("question 13 :", sumPolygon(3));
-
-//14 
-function howManyWalls(n, w, h) {
-    return n / (w * h);
-}
-console.log("question 14 :", Math.floor(howManyWalls(41, 3, 6)));
-
-//15
+//7. Let's Fuel Up!
 function calculateFuel(fuel) {
     if (fuel * 10 <=100) {
         return 100;
@@ -63,43 +37,106 @@ function calculateFuel(fuel) {
         return fuel * 10;
     }
 }
-console.log("question 15 :", calculateFuel(15));
+//console.log(calculateFuel(15));
 
-//16 
+//6. Sum of Polygon Angles
+function sumPolygon(n) {
+    if (n > 2) {
+        return (n-2)*180
+    } else {
+        return "nothing"
+    }    
+}
+//console.log(sumPolygon(6))
+
+//8. Moving House
 function yearsInOneHouse(age, moves) {
     return age / (moves + 1);
 }
-console.log("question 16 :", yearsInOneHouse(15, 2));
+//console.log(yearsInOneHouse(15, 2));
 
-//17 
-function makesTen(a, b) {
-    if ((a === 10 || b === 10) ||(a + b) === 10) {
-        return true;
-    } else {
-        return false;
-    }
-}
-console.log("question 17 :", makesTen(10, 9));
-
-//18 
+//9. Frames Per Second
 function frames(fps, num_per_sec) {
     return fps * (num_per_sec * 60);
 };
-console.log("question 18 :", frames(10, 25));
+//console.log(frames(10, 25));
 
-//19
-function calculator(exp) {
-return eval(exp);
-}
-console.log("question 19 :", calculator("49/7*2-3") )
 
-//20 
-function leapYear(years) {
-    if (years % 4 == 0) {
-        return true;
-    } else {
-        return false
-   }
-}
-console.log("question 20 :", leapYear(2021));
+
+
+
+
+
+
+
+
+
+
+// phần này không liên quan em làm cái khác :D
+
+// //8
+// function lessThan100(number1, number2) {
+//     if (number1 + number2 < 100) {
+//         return true
+//     } else {
+//         return false;
+//     }
+// };
+// console.log("question 8 :",lessThan100(50, 81));
+
+
+// //10 
+// function boolToString(flag) {
+//     return flag.toString();
+// }
+// console.log("question 10 :",boolToString(true))
+
+// //11 
+// function isSameNum(num1,num2) {
+//     if (num1 === num2) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log("question 11 :", isSameNum(2, 2))
+
+
+// //13 
+// function sumPolygon(angles) {
+//     return angles * 60;
+// }
+// console.log("question 13 :", sumPolygon(3));
+
+// //14 
+// function howManyWalls(n, w, h) {
+//     return n / (w * h);
+// }
+// console.log("question 14 :", Math.floor(howManyWalls(41, 3, 6)));
+
+// //17 
+// function makesTen(a, b) {
+//     if ((a === 10 || b === 10) ||(a + b) === 10) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log("question 17 :", makesTen(10, 9));
+
+// //19
+// function calculator(exp) {
+// return eval(exp);
+// }
+// console.log("question 19 :", calculator("49/7*2-3") )
+
+// //20 
+// function leapYear(years) {
+//     if (years % 4 == 0) {
+//         return true;
+//     } else {
+//         return false
+//    }
+// }
+// console.log("question 20 :", leapYear(2021));
 
