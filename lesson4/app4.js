b0bf0e2d1f5604cccc6ed81bf7c99b877435cbf8
Nhanhaiho <hainhan233@gmail.com>
2021-09-26 14:30:27 +0700

    bai hoc

---
+++ lesson4/app4.js
@@ -0,0 +1,119 @@
+//while loop
+// let i = prompt('nhap + - * /, nhap 0 de exit');
+// while (true ) {
+//     if (i == 0) { break };
+// };
+// console.log(i)
+
+
+//for loop
+// for (var i = 0; i <= 100; i++){
+    
+//     if (i %2==0) {
+//         continue;
+//     }
+//     console.log(i)
+// }   ;
+
+//Array
+// let Canh = "canh";
+// let Com = "com";
+// let Sua = "sua";
+//CREATE
+
+// console.log(food);
+// let Array = food.length
+// console.log(Array);
+
+//READ
+// let com = food[food.length - 1]
+// console.log(com);
+
+//UPDATE
+
+// food[2] = "tra";
+// console.log(food);
+
+// thêm phần tử vào mảng
+    //đầu mảng
+// food.unshift('thit cho');
+// console.log(food)
+    //cuối mảng
+// var newARR=food.push("con bo");
+// console.log(newARR);
+// console.log(food);
+
+// thêm ở bất kì vị trí nào của mảng;
+// food.splice(2, 1, "con chim");
+// console.log(food);
+
+//DELETE
+    //xóa đẩu mảng
+// food.shift();
+// console.log(food);
+    //xóa cuối mảng
+// food.pop();
+// console.log(food);
+    // xóa bất kì vị trí
+// food.splice(1, 2);
+// // console.log(food);
+// let food = ["canh", "com", "sua","che","chao",'con bo'];
+
+// for (var i = 0; i < food.length; i++) {
+   
+// }
+
+// let nums = [1, 2, 5, 7, 3, 9, -10];
+// // var Max = Math.max(...nums);
+// // console.log(Max)
+
+// function findMin(ArrayNum) {
+//     var min = 1;
+//     var n = ArrayNum.length;
+//     for (var i = 0; i < n; i++) {
+//         if (ArrayNum[i] < min) {
+//           min = ArrayNum[i]
+//       }
+//     }
+//     console.log(min)
+// }
+// findMin(nums);
+
+// function findMax(ArrayNum1) {
+//     var max = 1;
+//     var j = ArrayNum1.length;
+//     for (var i = 0; i < j; i++) {
+//         if (ArrayNum1[i] > max) {
+//             max=ArrayNum1[i]
+//         }
+//     }
+//     console.log(max)
+// }
+// findMax(nums);
+// nums = [-1, 4, 5, 123412, -8,0];
+
+// function pos(Array) {
+//     var posArr = [];
+//     var n = Array.length;
+//     for (var i = 0; i <n; i++) {
+//         if (Array[i] >= 0) {
+//             posArr.push(Array[i]);
+//         }
+//     }
+//  console.log(posArr)   
+// }
+// pos(nums)
+
+// function nev(Array) {
+//     var nevArr = [];
+//     var n = Array.length;
+//     for (var i = 0; i <n; i++) {
+//         if (Array[i] < 0) {
+//            nevArr.push(Array[i]);
+//         }
+//     }
+//  console.log(nevArr)   
+// }
+// nev(nums)
+
+
