e367bbb918e7081b242d0c4946afdff7d319c3f2
Nhanhaiho <hainhan233@gmail.com>
2021-10-10 21:26:17 +0700

    2 bai

---
+++ homework/lesson06/app6.js
@@ -0,0 +1,34 @@
+//1
+// const members = [
+// { name: 'Lan', gender: 'female' },
+// { name: 'Linh', gender: 'female' },
+// { name: 'Trung', gender: 'male' },
+// { name: 'Peter', gender: 'gay' }
+//  ];
+
+// members.forEach((peoples) => {
+//     if (peoples.gender != 'female') {
+//         console.log(peoples)
+//     }
+// })
+
+//4 
+// function freeShipping(obj) {
+//     var total = 0;
+//     for (var i in obj) {
+//         // console.log(obj[i]);
+//         total += obj[i]
+//     }
+//     // console.log(total);
+//     if (total >= 50) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// freeShipping({ "Shampoo": 5.99, "Rubber Ducks": 15.99 });
+// freeShipping({ "Monopoly": 11.99, "Secret Hitler": 35.99, "Bananagrams": 13.99 })
+
+
+
