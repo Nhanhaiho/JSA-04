735a5de50e99cd7c4d344e8ce061571623b0160f
Nhanhaiho <hainhan233@gmail.com>
2021-10-20 21:59:42 +0700

    cn17/10

---
+++ lesson7/app.js
@@ -0,0 +1,31 @@
+// const a = {
+//     html: {
+//         head: {
+//             meta: {},
+//             title: {
+//                 value: 'documment'
+//             },
+//         }
+//     }
+// }
+// console.log(a);
+// let h2Element = document.getElementById('title');
+// // console.log(h2Element);
+// // console.log(h2Element.id)
+// // console.log(h2Element.classList)
+// // console.log(h2Element.localName)
+// let red = document.getElementsByClassName('text-red');
+// for (var i of red) {
+//     console.log(i.innerText);
+//     console.log(i.localName);
+// }
+// const divEl = document.getElementById('contain');
+// console.log(divEl);
+// let inputEl = document.createElement("input");
+// inputEl.type = 'text';
+// inputEl.value = 'hello';
+// inputEl.className = 'nhan';
+// divEl.appendChild(inputEl);
+
+// const divEl = document.getElementById('contain');
+// console.log(divEl.children.remove())
