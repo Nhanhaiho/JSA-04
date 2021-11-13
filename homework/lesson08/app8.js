31efd2952e266a3509051131f48c93036750825e
Nhanhaiho <hainhan233@gmail.com>
2021-11-13 21:34:33 +0700

    xong bt basic

--- homework/lesson08/app8.js
+++ homework/lesson08/app8.js
@@ -24,29 +24,74 @@ var ulEl = document.querySelector('ul');
     
 // }
 
-btnaddEl.onclick = () => {
-    let userData = inputEl.value;
-    let getlocalStorage = localStorage.getItem('new todo')
-    if (getlocalStorage == null) {
-        listArr = [];
-    } else {
-        listArr =JSON.parse(getlocalStorage)
-    }
-    listArr.push(userData);
-    localStorage.setItem('new todo', JSON.stringify(listArr));
-    lists()
-}
 
-function lists() {
-    let getlocalStorage = localStorage.getItem('new todo');
-     if (getlocalStorage == null) {
-        listArr = [];
-    } else {
-        listArr =JSON.parse(getlocalStorage)
-    }
-    let newLi = '';
-    listArr.forEach((element, index) => {
-        newLi += `<li> ${element}<i class="fas fa-trash"></i></li>`;
+btnaddEl.addEventListener('click', function () {
+    // var btnTrash = document.createElement('button');
+    // console.log(btnTrash);
+    // var iTrash = document.createElement('i')
+    // iTrash.setAttribute("class", "fas fa-trash")
+    // btnTrash.appendChild(iTrash);
+    
+    //////////////////////////////////////////////
+    var liform = document.createElement('li');
+    
+    liform.innerHTML = input.value;
+    ulEl.appendChild(liform);
+    liform.addEventListener('click', function () {
+        liform.style.textDecoration = "line-through";
     })
-    ulEl.innerHTML = newLi;
-}
+     liform.addEventListener('dblclick', function () {
+         ulEl.removeChild(liform);
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btnaddEl.onclick = () => {
+//     let userData = inputEl.value;
+//     let getlocalStorage = localStorage.getItem('new todo')
+//     if (getlocalStorage == null) {
+//         listArr = [];
+//     } else {
+//         listArr =JSON.parse(getlocalStorage)
+//     }
+//     listArr.push(userData);
+//     localStorage.setItem('new todo', JSON.stringify(listArr));
+//     lists()
+// }
+
+// function lists() {
+//     let getlocalStorage = localStorage.getItem('new todo');
+//      if (getlocalStorage == null) {
+//         listArr = [];
+//     } else {
+//         listArr =JSON.parse(getlocalStorage)
+//     }
+//     let newLi = '';
+//     listArr.forEach((element, index) => {
+//         newLi += `<li> ${element}<i class="fas fa-trash"></i></li>`;
+//     })
+//     ulEl.innerHTML = newLi;
+// }
