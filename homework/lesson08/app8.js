a3c8fc82d9f3fdc4f0b5a85a35d8fbf8eb7b3efd
Nhanhaiho <hainhan233@gmail.com>
2021-11-23 09:34:49 +0700

    xong bai tap todo-list

--- homework/lesson08/app8.js
+++ homework/lesson08/app8.js
@@ -3,20 +3,34 @@ var inputEl = document.getElementById('input'); // element input
 var ulEl = document.querySelector('ul');
 
 const todos = [
-  { title: "Ăn tối", isDone: false, id: 1 },
-  { title: "Học bài", isDone: false, id: 2 },
-    { title: "Đi ngủ", isDone: true, id: 3 },
+//   { title: "Ăn tối", isDone: false, id: 1 },
+//   { title: "Học bài", isDone: false, id: 2 },
+//     { title: "Đi ngủ", isDone: false, id: 3 },
 ];
 
 // console.log('ban dau: ',todos)
 
 //  hàm để tạo ra những object
 function generatorTodoElements(todo) { // todo ở đây là mỗi object có trong mảng todos
+    const container = document.createElement('div');
+    container.style.display = 'flex';
+    container.style.justifyContent = "space-around";
+    container.style.alignItems = 'center';
+    // container.style.marginBottom = "10px";
+    
+    const deleteEl = document.createElement('span');
+    deleteEl.innerHTML = "x";
+    deleteEl.classList.add('deleteTodo') // cho một cái class lát chọn để thực hiện chức năng 
+    deleteEl.setAttribute('dataId',todo.id);
+
     const liEls = document.createElement('li'); // tạo thẻ li 
     liEls.setAttribute('class', todo.isDone ? 'todoItems done' : 'todoItems'); // kt xem isdone true or false
     liEls.setAttribute('dataId',todo.id)
     liEls.textContent = todo.title; // nội dung thẻ li
-    return liEls;
+    
+    container.appendChild(liEls)
+    container.appendChild(deleteEl)
+    return container;
     
 }
 
@@ -27,11 +41,11 @@ function renderTodos(todosData) { //truyền vào một array giống arr todos 
         ulEl.appendChild(todoHtml); // append vào trong thẻ ul
        
     }
-
     liTodo() // phải gọi lại hàm này thì nó mới hiểu và chạy cho những lần sau 
+    removeTodo() // gọi lại hàm để một khi render là chạy được
 }
 
-// viết hàm để lấy những thẻ li 
+// viết hàm để lấy update li 
 function liTodo() {
     const liItems = document.querySelectorAll(".todoItems");
     // console.log(liItems);
@@ -46,10 +60,36 @@ function liTodo() {
             
             rerenderTodos()
         })
+        
     })
     
 }
+// ham delete
+function removeTodo() {
+    const liEls = document.querySelectorAll(".deleteTodo");
+    for (let item of liEls) {
+        item.addEventListener('click', () => {
+            let todoId = item.getAttribute('dataId'); // lấy data id trên mảng todos khi dblc vào li
+            for (let i = 0; i < todos.length; i++) {
+                if (todos[i].id == todoId) {
+                    todos.splice(i, 1);
+                    console.log(todos)
+                    rerenderTodos()
+                    break;
+                    
+                }
+            }
+            })
+            
+            
+        }
+   }
 
+
+
+
+
+// tránh trường hợp bị nhảy lại 
 function rerenderTodos() {
      ulEl.innerHTML = "";
         renderTodos(todos); // gọi làm hàm
@@ -75,6 +115,8 @@ btnaddEl.addEventListener('click', () => {
     }
     
 });
+
+
 
 
 
